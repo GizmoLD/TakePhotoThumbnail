91307f9859d79846401ce7a924d5802d25181af1
GizmoLD <122391569+GizmoLD@users.noreply.github.com>
2024-03-13 22:53:55 +0100

    Final

--- app/src/main/java/com/example/takephotothumbnail/MainActivity.java
+++ app/src/main/java/com/example/takephotothumbnail/MainActivity.java
@@ -5,22 +5,36 @@ import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
+    private static final String[] CAMERA_PERMISSION = new String[]{android.Manifest.permission.CAMERA};
+    private static final int CAMERA_REQUEST_CODE = 10;
     private Button button;
     private Button galeria;
     private ImageView imageView;
     private ActivityResultLauncher<Intent> takePictureLauncher;
     private ActivityResultLauncher<Intent> pickImageLauncher;
+    private String currentPhotoPath;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,17 +44,17 @@ public class MainActivity extends AppCompatActivity {
         button = findViewById(R.id.button);
         galeria = findViewById(R.id.galeria);
 
+        requestPermission();
+
         // Lanzador para la captura de fotos
-        takePictureLauncher = registerForActivityResult(
-                new ActivityResultContracts.StartActivityForResult(),
-                new ActivityResultCallback<ActivityResult>() {
-                    @Override
-                    public void onActivityResult(ActivityResult result) {
-                        if (result.getResultCode() == RESULT_OK) {
-                            Intent data = result.getData();
-                            Bundle extras = data.getExtras();
-                            Bitmap imageBitmap = (Bitmap) extras.get("data");
-                            imageView.setImageBitmap(imageBitmap);
+        takePictureLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == RESULT_OK) {
+                        Intent data = result.getData();
+                        if (data != null) {
+                            String imagePath = data.getStringExtra("imagePath");
+                            ImageView imageView = findViewById(R.id.imageView);
+                            imageView.setImageURI(Uri.parse(imagePath));
                         }
                     }
                 });
@@ -55,7 +69,6 @@ public class MainActivity extends AppCompatActivity {
                             // There are no request codes
                             Intent data = result.getData();
                             Uri uri = data.getData();
-                            ImageView imageView = findViewById(R.id.imageView);
                             imageView.setImageURI(uri);
                         }
                     }
@@ -64,7 +77,11 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                dispatchTakePictureIntent();
+                if (hasCameraPermission()) {
+                    enableCamera();
+                } else {
+                    requestPermission();
+                }
             }
         });
 
@@ -76,10 +93,22 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-        takePictureLauncher.launch(takePictureIntent);
+    private File createImageFile() throws IOException {
+        // Crear un nombre de archivo único
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Guardar la ruta del archivo para su uso posterior
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
     }
+
 
     public void openSomeActivityForResult() {
         // Crear Intent para seleccionar imágenes desde la galería
@@ -88,5 +117,24 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
         // Lanzar actividad para obtener el resultado
         pickImageLauncher.launch(intent);
+    }
+
+    private boolean hasCameraPermission() {
+        return ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.CAMERA
+        ) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(
+                this,
+                CAMERA_PERMISSION,
+                CAMERA_REQUEST_CODE);
+    }
+
+    private void enableCamera() {
+        Intent intent = new Intent(this, CameraActivity.class);
+        takePictureLauncher.launch(intent);
     }
 }
